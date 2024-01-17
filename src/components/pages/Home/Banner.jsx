220b2afaf6b4e64dcf67f11a5757bf0c384bb44b
Mo Hazzad <hazzadmdap@gmail.com>
2024-01-17 12:58:51 +0300

    added herro section texts

--- src/components/pages/Home/Banner.jsx
+++ src/components/pages/Home/Banner.jsx
@@ -1,5 +1,6 @@
 import bannerImagTwo from "../../../assets/appartment Slider.jpg";
 import bannerImagOne from "../../../assets/familySlider .jpg";
+import './home.css'
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation } from "swiper/modules";
@@ -9,27 +10,32 @@ import "swiper/css/navigation";
 import "../About/slider.css";
 import { Link } from "react-router-dom";
 import i18next from "i18next";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
+import { useTranslation } from "react-i18next";
 // import i18next from "i18next";
 // import { useEffect } from "react";
 // import AOS from 'aos';
 
 
 const Banner = () => {
-  const [languageKey, setLanguageKey] = useState(i18next.language);
+  // const [languageKey, setLanguageKey] = useState(i18next.language);
+  const currentLanguage = i18next.language;
+  const { t } = useTranslation("home");
 
-  useEffect(() => {
-    const handleLanguageChange = (lng) => {
-      setLanguageKey(lng);
-    };
+  // console.log(languageKey,'key');
+  console.log(currentLanguage,'current lang');
+  // useEffect(() => {
+  //   const handleLanguageChange = (lng) => {
+  //     setLanguageKey(lng);
+  //   };
 
-    i18next.on('languageChanged', handleLanguageChange);
+  //   i18next.on('languageChanged', handleLanguageChange);
 
-    return () => {
-      i18next.off('languageChanged', handleLanguageChange);
-    };
-  }, []);
+  //   return () => {
+  //     i18next.off('languageChanged', handleLanguageChange);
+  //   };
+  // }, []);
  
 
   return (
@@ -42,8 +48,8 @@ const Banner = () => {
           className={` h-[calc(100vh-74px)] `}
           autoplay={{ delay: 3000 }}
           loop={true}
-          dir={!languageKey ? "rtl" : "ltr" }
-          key={languageKey}
+          dir={!currentLanguage ? "rtl" : "ltr" }
+          key={currentLanguage}
         >
           <SwiperSlide
             className="bg-cover bg-center relative"
@@ -57,20 +63,16 @@ const Banner = () => {
             />
             {/* <div className='absolute top-0 left-0 w-full h-full bg-black opacity-50'></div> */}
             <div
-              className={`text-content w-full md:w-[60%] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-white `}
+              className={`text-content w-full md:w-[80%] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-white `}
             >
-              <div className="text-center flex flex-col gap-2 md:gap-4 uppercase">
-                <p className="text-[8px] md:text-sm tracking-widest ">
-                  Luxury Hotel & Best Resort{" "}
-                </p>
-                <h1 className=" text-3xl md:text-7xl  ">Enjoy A Luxury</h1>
-                <h1 className="text-3xl md:text-7xl tracking-widest ">
-                  Experience
-                </h1>
+              <div className="text-center flex flex-col gap-2 md:gap-3 uppercase">
+                <p className="text-[10px] md:text-[20px] tracking-widest "> {t("headerSemiTitle")} </p>
+                <h1 className={`hero-text ${currentLanguage === 'ar' ? 'arabic' : 'english'}`}>{t("titleHero")}</h1>
+                <h1 className={`hero-text ${currentLanguage === 'ar' ? 'arabic' : 'english'}`}>{t("subTitleHero")}</h1>
 
                 <div className="py-3">
                   <Link to={'/roomSearch'} className=" py-2 md:py-3  text-xs md:text-sm  px-4 border  md:px-8 md:border tracking-widest heroText">
-                    Rooms & Suites
+                    {t("heroButton")}
                   </Link>
                 </div>
               </div>
@@ -86,22 +88,18 @@ const Banner = () => {
               src={bannerImagOne}
               alt=""
             />
-            {/* <div className='absolute top-0 left-0 w-full h-full bg-black opacity-50'></div> */}
+            
             <div
               className={`text-content w-full md:w-[60%] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-white `}
             >
               <div className="text-center flex flex-col gap-2 md:gap-4 uppercase">
-                <p className="text-[8px] md:text-sm tracking-widest ">
-                  Luxury Hotel & Best Resort{" "}
-                </p>
-                <h1 className=" text-3xl md:text-7xl  ">Enjoy A Luxury</h1>
-                <h1 className="text-3xl md:text-7xl tracking-widest ">
-                  Experience
-                </h1>
+              <p className="text-[10px] md:text-[20px] tracking-widest "> {t("headerSemiTitleTwo")} </p>
+                <h1 className={`hero-text ${currentLanguage === 'ar' ? 'arabic' : 'english'}`}>{t("titleSecondImage")}</h1>
+                <h1 className={`hero-text ${currentLanguage === 'ar' ? 'arabic' : 'english'}`}>{t("subTitleHeroSecond")}</h1>
 
                 <div className="py-3">
                   <Link to={'/roomSearch'} className=" py-2 md:py-3  text-xs md:text-sm  px-4 border  md:px-8 md:border tracking-widest heroText">
-                    Rooms & Suites
+                    {t("heroButton")}
                   </Link>
                 </div>
               </div>
