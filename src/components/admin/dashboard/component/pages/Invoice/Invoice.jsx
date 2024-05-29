2e3e29921b7de626be02f35b56453a97cf7ec152
Mo Hazzad <hazzadmdap@gmail.com>
2024-05-29 22:05:59 +0300

    production

--- src/components/admin/dashboard/component/pages/Invoice/Invoice.jsx
+++ src/components/admin/dashboard/component/pages/Invoice/Invoice.jsx
@@ -32,7 +32,8 @@ const Invoice = () => {
 
         const response = await axios.get(
           // `https://type-script-server.vercel.app/api/booking/${userEmail}?lang=${currentLanguage}`,
-          `http://localhost:5000/api/booking/invoice/${bookingId}?lang=${currentLanguage}`,
+          `https://type-script-server.vercel.app/api/booking/invoice/${bookingId}?lang=${currentLanguage}`,
+          // `http://localhost:5000/api/booking/invoice/${bookingId}?lang=${currentLanguage}`,
           {
             headers: {
               Authorization: `${userToken}`,
