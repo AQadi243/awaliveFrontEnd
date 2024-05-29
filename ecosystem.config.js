678a931f41136cd7d65516ed46ad94b82918af33
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2024-05-29 15:11:19 -0400

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [{
+    name: 'my-next-app',
+    script: 'npm',
+    args: 'start',
+    watch: true,
+    ignore_watch: ["node_modules"],
+    env: {
+      NODE_ENV: 'production',
+    }
+  }]
+};
