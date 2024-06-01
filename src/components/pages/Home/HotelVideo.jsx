03c7feb2a46df42fec114f077d37da6aacee1785
Mo Hazzad <hazzadmdap@gmail.com>
2024-06-01 21:09:17 +0300

    video frame changed

--- src/components/pages/Home/HotelVideo.jsx
+++ src/components/pages/Home/HotelVideo.jsx
@@ -2,7 +2,7 @@
 const HotelVideo = () => {
   return (
     <section className="max-w-[1080px] mx-auto py-10">
-      <div className="relative overflow-hidden pb-[56.25%] md:pb-[35.25%] h-0">
+      <div className="relative overflow-hidden pb-[56.25%] md:pb-[40.25%] h-0">
         <iframe
           className="absolute top-0 left-0 w-full h-full"
           src="https://www.youtube.com/embed/2AD40BCejhI?autoplay=1&loop=1&playlist=2AD40BCejhI&si=GHalrh97fQnIpMFD"
