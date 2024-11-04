7d778a79c9ef30cbc56b34e9f7f0bb9ef587543e
Abdullah Qadi <y>
2024-11-04 17:59:09 +0300

    Initial commit

---
+++ src/components/admin/dashboard/component/pages/Rooms/AllRooms/PermanentDeleteRoomButton.jsx
@@ -0,0 +1,69 @@
+import { Popconfirm, message } from 'antd';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+
+const PermanentDeleteRoomButton = ({onUpdate,roomId}) => {
+  
+
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    message.error("No token found, authentication failed.");
+    return null;
+  }
+
+  const decodedToken = jwtDecode(token);
+  const currentTime = Date.now() / 1000; // Current time in seconds
+
+  if (decodedToken.exp < currentTime) {
+    message.error("Token has expired.");
+    return null;
+  }
+
+  if (decodedToken.role !== "admin") {
+    message.error("You are not authorized to perform this action.");
+    return null;
+  }
+  const deleteRoomPermanently = async () => {
+    try {
+      // await axios.delete(`https://server.awalivhotel.com/api/room/${roomId}/permanent`, {
+      await axios.delete(`${import.meta.env.VITE_API_URL}/room/${roomId}/permanent`, {
+        headers: {
+          Authorization: `${token}`,
+        },
+      });
+      message.success("Room deleted permanently.");
+      onUpdate(); // Trigger a refetch
+    } catch (error) {
+      console.error("Error during permanent room deletion:", error.response);
+      message.error("Failed to delete the room permanently.");
+    }
+  };
+
+
+  
+  const confirm = () => {
+    
+      deleteRoomPermanently();
+    
+  };
+  return (
+    <Popconfirm
+          title="Warning"
+          description="Are you sure you want to delete this room permanently?"
+          onConfirm={confirm}
+          okText="Delete Permanently"
+          cancelText="Cancel"
+          placement="left" 
+        >
+          <button className="hover:bg-slate-50 flex justify-center items-center text-xs uppercase font-semibold text-red-500">
+            Delete Permanently
+          </button>
+        </Popconfirm>
+     
+
+
+  )
+}
+
+export default PermanentDeleteRoomButton
